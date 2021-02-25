6bf78194ca2f4afd3ffd51d8309b071e118217f8
koualsky <koualsky@gmail.com>
2021-02-25 11:31:43 +0100

    Added all API functionality

--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import Book, Opinion
 
-# Create your views here.
+
+def books(request):
+    MAX = 20
+    if request.GET.get("pk"):
+        given_pk = request.GET.get("pk")
+        book = get_object_or_404(Book, pk=given_pk)
+        opinions = Opinion.objects.filter(book=book)
+        data = {
+            "results": {
+                "pk": book.pk,
+                "author": book.author,
+                "title": book.title,
+                "type": book.type,
+                "isbn": book.isbn,
+                "opinions": list(
+                    opinions.values("pk", "book", "rate", "description", "isbn")
+                ),
+            }
+        }
+    elif request.GET.get("title"):
+        given_title = request.GET.get("title")
+        books = Book.objects.filter(title__contains=given_title)
+        data = {"results": list(books.values("pk", "author", "title", "type", "isbn"))}
+    else:
+        books = Book.objects.all()[:MAX]
+        data = {"results": list(books.values("pk", "author", "title", "type", "isbn"))}
+    return JsonResponse(data)
