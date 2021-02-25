a76b4560b9030db88366e27a589bb182a7231555
koualsky <koualsky@gmail.com>
2021-02-25 10:31:39 +0100

    Added models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Book, Opinion
 
-# Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Opinion)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    isbn = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Opinion(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='opinions')
+    rate = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    isbn = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return f'({str(self.book)}) {self.description}'
